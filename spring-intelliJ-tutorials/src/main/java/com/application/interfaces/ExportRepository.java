ec82b8cd76bbcc983c304aa66b3bd973680f160f
Trần Hoàng Kim Anh <kimanhtranhoang@gmail.com>
2024-04-23 02:37:09 +0700

    Delete spring-intelliJ-tutorials directory

--- spring-intelliJ-tutorials/src/main/java/com/application/interfaces/ExportRepository.java
+++
@@ -1,9 +0,0 @@
-package com.application.interfaces;
-
-import com.application.entities.Export;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ExportRepository extends JpaRepository<Export, Long> {
-}
